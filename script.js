0da0c92efb92a74c108b40dbf124a2781d942fc3
Manindra Roy <155424727+Manindra-Roy@users.noreply.github.com>
2024-11-05 20:56:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,472 @@
+const _0x1fd3f6 = _0x14b5;
+(function (_0x405c49, _0x4d5cd2) {
+  const _0x4ead33 = _0x14b5,
+    _0x58ddcf = _0x405c49();
+  while (!![]) {
+    try {
+      const _0x94c582 =
+        (parseInt(_0x4ead33(0x1c7)) / 0x1) *
+          (-parseInt(_0x4ead33(0x1b1)) / 0x2) +
+        (parseInt(_0x4ead33(0x1bc)) / 0x3) *
+          (-parseInt(_0x4ead33(0x1c1)) / 0x4) +
+        (-parseInt(_0x4ead33(0x1ac)) / 0x5) *
+          (parseInt(_0x4ead33(0x1d0)) / 0x6) +
+        (parseInt(_0x4ead33(0x1ba)) / 0x7) *
+          (parseInt(_0x4ead33(0x186)) / 0x8) +
+        parseInt(_0x4ead33(0x17f)) / 0x9 +
+        parseInt(_0x4ead33(0x198)) / 0xa +
+        parseInt(_0x4ead33(0x1be)) / 0xb;
+      if (_0x94c582 === _0x4d5cd2) break;
+      else _0x58ddcf["push"](_0x58ddcf["shift"]());
+    } catch (_0x1728fb) {
+      _0x58ddcf["push"](_0x58ddcf["shift"]());
+    }
+  }
+})(_0x4f4b, 0xaccf2);
+let pageLogo = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1cb)),
+  heroBtn = document["querySelector"](_0x1fd3f6(0x1b3)),
+  homePage = document[_0x1fd3f6(0x19e)](".hero"),
+  educationBtn = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1d5)),
+  educationPage = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x17d)),
+  skillsBtn = document["querySelector"](_0x1fd3f6(0x19d)),
+  skillsPage = document[_0x1fd3f6(0x19e)](".skills"),
+  servicesBtn = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x188)),
+  servicesPage = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1dc)),
+  contactBtnNav = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1b0)),
+  contactPage = document["querySelector"](_0x1fd3f6(0x1a3)),
+  leftDoor = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x193)),
+  rightDoor = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1b5)),
+  htmlBar = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x182)),
+  htmlPersent = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1da)),
+  cssBar = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1ab)),
+  cssPersent = document["querySelector"](".css-persent>p"),
+  jsBar = document["querySelector"](".js-bar"),
+  jsPersent = document["querySelector"](_0x1fd3f6(0x1a6)),
+  reactBar = document[_0x1fd3f6(0x19e)](".react-bar"),
+  reactPersent = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1a0)),
+  nodejsBar = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1d4)),
+  nodejsPersent = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x18c)),
+  pythonBar = document["querySelector"](_0x1fd3f6(0x1c5)),
+  pythonPersent = document[_0x1fd3f6(0x19e)](".python-persent"),
+  djangoBar = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x19f)),
+  djangoPersent = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1cc)),
+  cBar = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x17b)),
+  cPersent = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1d3)),
+  gitBar = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1c4)),
+  gitPersent = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1c8)),
+  circleProBar = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1bf));
+function _0x14b5(_0x4319e1, _0x40e26b) {
+  const _0x4f4b56 = _0x4f4b();
+  return (
+    (_0x14b5 = function (_0x14b5b5, _0x192440) {
+      _0x14b5b5 = _0x14b5b5 - 0x177;
+      let _0xb268a7 = _0x4f4b56[_0x14b5b5];
+      return _0xb268a7;
+    }),
+    _0x14b5(_0x4319e1, _0x40e26b)
+  );
+}
+const skillsAnimations1 = () => {
+    const _0x22a4ec = _0x1fd3f6;
+    htmlBar[_0x22a4ec(0x19b)](_0x22a4ec(0x187), _0x22a4ec(0x1ad)),
+      cssBar[_0x22a4ec(0x19b)](_0x22a4ec(0x187), _0x22a4ec(0x1d1)),
+      jsBar[_0x22a4ec(0x19b)](_0x22a4ec(0x187), _0x22a4ec(0x1c9)),
+      reactBar[_0x22a4ec(0x19b)](_0x22a4ec(0x187), _0x22a4ec(0x1aa)),
+      nodejsBar[_0x22a4ec(0x19b)](
+        _0x22a4ec(0x187),
+        "animation-name:nodejs-pro-bar-var-1;"
+      ),
+      pythonBar[_0x22a4ec(0x19b)](
+        _0x22a4ec(0x187),
+        "animation-name:python-pro-bar-var-1;"
+      ),
+      djangoBar["setAttribute"]("style", _0x22a4ec(0x178)),
+      cBar["setAttribute"](_0x22a4ec(0x187), "animation-name:c-pro-bar-var-1;"),
+      gitBar[_0x22a4ec(0x19b)](_0x22a4ec(0x187), _0x22a4ec(0x1de)),
+      circleProBar[_0x22a4ec(0x18b)][_0x22a4ec(0x1bd)](_0x22a4ec(0x1ce)),
+      circleProBar[_0x22a4ec(0x18b)][_0x22a4ec(0x195)](_0x22a4ec(0x180));
+  },
+  skillsAnimations2 = () => {
+    const _0x5db217 = _0x1fd3f6;
+    htmlBar[_0x5db217(0x19b)](_0x5db217(0x187), _0x5db217(0x1d6)),
+      cssBar[_0x5db217(0x19b)](
+        _0x5db217(0x187),
+        "animation-name:css-pro-bar-var-2;"
+      ),
+      jsBar["setAttribute"]("style", _0x5db217(0x179)),
+      reactBar[_0x5db217(0x19b)](_0x5db217(0x187), _0x5db217(0x177)),
+      nodejsBar[_0x5db217(0x19b)](_0x5db217(0x187), _0x5db217(0x1cd)),
+      pythonBar["setAttribute"](_0x5db217(0x187), _0x5db217(0x1dd)),
+      djangoBar[_0x5db217(0x19b)](
+        _0x5db217(0x187),
+        "animation-name:django-pro-bar-var-2;"
+      ),
+      cBar[_0x5db217(0x19b)]("style", _0x5db217(0x1c6)),
+      gitBar["setAttribute"](_0x5db217(0x187), _0x5db217(0x192)),
+      circleProBar[_0x5db217(0x18b)][_0x5db217(0x1bd)](_0x5db217(0x180)),
+      circleProBar["classList"]["remove"]("progressbar-animation-1");
+  },
+  setHome01 = () => {
+    const _0x39c88b = _0x1fd3f6;
+    pageLogo[_0x39c88b(0x19b)](_0x39c88b(0x1a5), _0x39c88b(0x1bb)),
+      contactPage[_0x39c88b(0x19b)]("style", _0x39c88b(0x1ae)),
+      servicesPage["setAttribute"](_0x39c88b(0x187), _0x39c88b(0x1ae)),
+      skillsPage[_0x39c88b(0x19b)](_0x39c88b(0x187), _0x39c88b(0x1ae)),
+      educationPage["setAttribute"]("style", "z-index:\x20-1;"),
+      leftDoor[_0x39c88b(0x19b)](_0x39c88b(0x187), _0x39c88b(0x1a2)),
+      rightDoor[_0x39c88b(0x19b)]("style", "animation-name:right-door-01;"),
+      homePage[_0x39c88b(0x19b)](_0x39c88b(0x187), "z-index:\x201;");
+  },
+  setHome02 = () => {
+    const _0xcc1025 = _0x1fd3f6;
+    pageLogo[_0xcc1025(0x19b)](_0xcc1025(0x1a5), _0xcc1025(0x1b7)),
+      contactPage[_0xcc1025(0x19b)](_0xcc1025(0x187), "z-index:\x20-1;"),
+      servicesPage[_0xcc1025(0x19b)](_0xcc1025(0x187), _0xcc1025(0x1ae)),
+      skillsPage[_0xcc1025(0x19b)](_0xcc1025(0x187), _0xcc1025(0x1ae)),
+      educationPage[_0xcc1025(0x19b)](_0xcc1025(0x187), "z-index:\x20-1;"),
+      leftDoor[_0xcc1025(0x19b)](_0xcc1025(0x187), _0xcc1025(0x18a)),
+      rightDoor[_0xcc1025(0x19b)](_0xcc1025(0x187), _0xcc1025(0x1c2)),
+      homePage["setAttribute"](_0xcc1025(0x187), _0xcc1025(0x1d7));
+  },
+  setHome1 = () => {
+    const _0x233e8c = _0x1fd3f6;
+    heroBtn[_0x233e8c(0x19b)](_0x233e8c(0x1a5), _0x233e8c(0x1c0)),
+      contactPage["setAttribute"](_0x233e8c(0x187), _0x233e8c(0x1ae)),
+      servicesPage[_0x233e8c(0x19b)](_0x233e8c(0x187), _0x233e8c(0x1ae)),
+      skillsPage[_0x233e8c(0x19b)](_0x233e8c(0x187), _0x233e8c(0x1ae)),
+      educationPage["setAttribute"]("style", _0x233e8c(0x1ae)),
+      leftDoor[_0x233e8c(0x19b)](_0x233e8c(0x187), _0x233e8c(0x1db)),
+      rightDoor[_0x233e8c(0x19b)]("style", "animation-name:right-door-1;"),
+      homePage[_0x233e8c(0x19b)](_0x233e8c(0x187), _0x233e8c(0x1d7)),
+      openMenuBar2();
+  },
+  setHome2 = () => {
+    const _0x4fb87a = _0x1fd3f6;
+    heroBtn[_0x4fb87a(0x19b)]("onclick", _0x4fb87a(0x199)),
+      contactPage[_0x4fb87a(0x19b)](_0x4fb87a(0x187), _0x4fb87a(0x1ae)),
+      servicesPage["setAttribute"](_0x4fb87a(0x187), "z-index:\x20-1;"),
+      skillsPage[_0x4fb87a(0x19b)](_0x4fb87a(0x187), _0x4fb87a(0x1ae)),
+      educationPage[_0x4fb87a(0x19b)](_0x4fb87a(0x187), "z-index:\x20-1;"),
+      leftDoor[_0x4fb87a(0x19b)](_0x4fb87a(0x187), _0x4fb87a(0x1b9)),
+      rightDoor[_0x4fb87a(0x19b)](_0x4fb87a(0x187), _0x4fb87a(0x184)),
+      homePage[_0x4fb87a(0x19b)](_0x4fb87a(0x187), _0x4fb87a(0x1d7)),
+      openMenuBar2();
+  },
+  setEdu1 = () => {
+    const _0xbc1b1e = _0x1fd3f6;
+    educationBtn[_0xbc1b1e(0x19b)]("onclick", "setEdu2()"),
+      contactPage[_0xbc1b1e(0x19b)](_0xbc1b1e(0x187), _0xbc1b1e(0x1ae)),
+      servicesPage["setAttribute"]("style", _0xbc1b1e(0x1ae)),
+      skillsPage["setAttribute"](_0xbc1b1e(0x187), _0xbc1b1e(0x1ae)),
+      homePage[_0xbc1b1e(0x19b)]("style", "z-index:\x20-1;"),
+      leftDoor[_0xbc1b1e(0x19b)](_0xbc1b1e(0x187), _0xbc1b1e(0x1c3)),
+      rightDoor[_0xbc1b1e(0x19b)](_0xbc1b1e(0x187), _0xbc1b1e(0x1ca)),
+      educationPage[_0xbc1b1e(0x19b)](_0xbc1b1e(0x187), "z-index:\x201;"),
+      openMenuBar2();
+  },
+  setEdu2 = () => {
+    const _0x47469d = _0x1fd3f6;
+    educationBtn[_0x47469d(0x19b)](_0x47469d(0x1a5), "setEdu1()"),
+      contactPage[_0x47469d(0x19b)](_0x47469d(0x187), "z-index:\x20-1;"),
+      servicesPage["setAttribute"]("style", _0x47469d(0x1ae)),
+      skillsPage["setAttribute"](_0x47469d(0x187), "z-index:\x20-1;"),
+      homePage[_0x47469d(0x19b)](_0x47469d(0x187), _0x47469d(0x1ae)),
+      leftDoor[_0x47469d(0x19b)](
+        _0x47469d(0x187),
+        "animation-name:left-door-4;"
+      ),
+      rightDoor[_0x47469d(0x19b)](
+        _0x47469d(0x187),
+        "animation-name:right-door-4;"
+      ),
+      educationPage[_0x47469d(0x19b)](_0x47469d(0x187), _0x47469d(0x1d7)),
+      openMenuBar2();
+  },
+  setSkill1 = () => {
+    const _0x2b57c6 = _0x1fd3f6;
+    skillsBtn[_0x2b57c6(0x19b)](_0x2b57c6(0x1a5), _0x2b57c6(0x1d9)),
+      contactPage[_0x2b57c6(0x19b)](_0x2b57c6(0x187), _0x2b57c6(0x1ae)),
+      servicesPage["setAttribute"](_0x2b57c6(0x187), _0x2b57c6(0x1ae)),
+      educationPage["setAttribute"](_0x2b57c6(0x187), _0x2b57c6(0x1ae)),
+      homePage[_0x2b57c6(0x19b)]("style", _0x2b57c6(0x1ae)),
+      leftDoor[_0x2b57c6(0x19b)](_0x2b57c6(0x187), _0x2b57c6(0x17e)),
+      rightDoor["setAttribute"](_0x2b57c6(0x187), _0x2b57c6(0x190)),
+      skillsPage[_0x2b57c6(0x19b)]("style", _0x2b57c6(0x1d7)),
+      skillsAnimations1(),
+      openMenuBar2();
+  },
+  setSkill2 = () => {
+    const _0x11c615 = _0x1fd3f6;
+    skillsBtn[_0x11c615(0x19b)](_0x11c615(0x1a5), "setSkill1()"),
+      contactPage[_0x11c615(0x19b)](_0x11c615(0x187), _0x11c615(0x1ae)),
+      servicesPage[_0x11c615(0x19b)]("style", _0x11c615(0x1ae)),
+      educationPage[_0x11c615(0x19b)]("style", _0x11c615(0x1ae)),
+      homePage[_0x11c615(0x19b)](_0x11c615(0x187), _0x11c615(0x1ae)),
+      leftDoor[_0x11c615(0x19b)]("style", _0x11c615(0x185)),
+      rightDoor[_0x11c615(0x19b)](_0x11c615(0x187), _0x11c615(0x17c)),
+      skillsPage[_0x11c615(0x19b)](_0x11c615(0x187), _0x11c615(0x1d7)),
+      skillsAnimations2(),
+      openMenuBar2();
+  },
+  setServ1 = () => {
+    const _0x12098a = _0x1fd3f6;
+    servicesBtn["setAttribute"]("onclick", "setServ2()"),
+      contactPage[_0x12098a(0x19b)]("style", _0x12098a(0x1ae)),
+      skillsPage["setAttribute"](_0x12098a(0x187), _0x12098a(0x1ae)),
+      educationPage[_0x12098a(0x19b)]("style", _0x12098a(0x1ae)),
+      homePage[_0x12098a(0x19b)](_0x12098a(0x187), "z-index:\x20-1;"),
+      leftDoor[_0x12098a(0x19b)](_0x12098a(0x187), _0x12098a(0x18e)),
+      rightDoor[_0x12098a(0x19b)](_0x12098a(0x187), _0x12098a(0x196)),
+      servicesPage[_0x12098a(0x19b)](_0x12098a(0x187), _0x12098a(0x1d7)),
+      openMenuBar2();
+  },
+  setServ2 = () => {
+    const _0x4e39ca = _0x1fd3f6;
+    servicesBtn[_0x4e39ca(0x19b)](_0x4e39ca(0x1a5), "setServ1()"),
+      contactPage[_0x4e39ca(0x19b)]("style", _0x4e39ca(0x1ae)),
+      skillsPage[_0x4e39ca(0x19b)](_0x4e39ca(0x187), _0x4e39ca(0x1ae)),
+      educationPage[_0x4e39ca(0x19b)](_0x4e39ca(0x187), _0x4e39ca(0x1ae)),
+      homePage["setAttribute"](_0x4e39ca(0x187), "z-index:\x20-1;"),
+      leftDoor[_0x4e39ca(0x19b)](
+        _0x4e39ca(0x187),
+        "animation-name:left-door-8;"
+      ),
+      rightDoor["setAttribute"](_0x4e39ca(0x187), _0x4e39ca(0x1a8)),
+      servicesPage[_0x4e39ca(0x19b)](_0x4e39ca(0x187), _0x4e39ca(0x1d7)),
+      openMenuBar2();
+  };
+let delayInMilliseconds = 0x5dc;
+const setContact1 = () => {
+    const _0x372bc3 = _0x1fd3f6;
+    contactBtnNav[_0x372bc3(0x19b)]("onclick", _0x372bc3(0x181)),
+      servicesPage[_0x372bc3(0x19b)](_0x372bc3(0x187), _0x372bc3(0x1ae)),
+      skillsPage["setAttribute"]("style", _0x372bc3(0x1ae)),
+      educationPage[_0x372bc3(0x19b)](_0x372bc3(0x187), "z-index:\x20-1;"),
+      homePage["setAttribute"]("style", _0x372bc3(0x1ae)),
+      leftDoor[_0x372bc3(0x19b)]("style", _0x372bc3(0x183)),
+      rightDoor["setAttribute"]("style", _0x372bc3(0x189)),
+      contactPage[_0x372bc3(0x19b)](_0x372bc3(0x187), _0x372bc3(0x1d7)),
+      openMenuBar2(),
+      setTimeout(function () {
+        switchToContact();
+      }, delayInMilliseconds);
+  },
+  setContact2 = () => {
+    const _0x41ad0e = _0x1fd3f6;
+    contactBtnNav[_0x41ad0e(0x19b)](_0x41ad0e(0x1a5), "setContact1()"),
+      servicesPage[_0x41ad0e(0x19b)](_0x41ad0e(0x187), _0x41ad0e(0x1ae)),
+      skillsPage["setAttribute"](_0x41ad0e(0x187), _0x41ad0e(0x1ae)),
+      educationPage[_0x41ad0e(0x19b)](_0x41ad0e(0x187), _0x41ad0e(0x1ae)),
+      homePage["setAttribute"](_0x41ad0e(0x187), _0x41ad0e(0x1ae)),
+      leftDoor["setAttribute"]("style", _0x41ad0e(0x17a)),
+      rightDoor[_0x41ad0e(0x19b)](_0x41ad0e(0x187), _0x41ad0e(0x197)),
+      contactPage[_0x41ad0e(0x19b)](_0x41ad0e(0x187), _0x41ad0e(0x1d7)),
+      openMenuBar2(),
+      setTimeout(function () {
+        switchToContact();
+      }, delayInMilliseconds);
+  },
+  setContact3 = () => {
+    const _0x316f19 = _0x1fd3f6;
+    contactBtnNav["setAttribute"](_0x316f19(0x1a5), _0x316f19(0x181)),
+      servicesPage[_0x316f19(0x19b)](_0x316f19(0x187), _0x316f19(0x1ae)),
+      skillsPage[_0x316f19(0x19b)]("style", _0x316f19(0x1ae)),
+      educationPage["setAttribute"]("style", _0x316f19(0x1ae)),
+      homePage[_0x316f19(0x19b)]("style", _0x316f19(0x1ae)),
+      leftDoor[_0x316f19(0x19b)](
+        _0x316f19(0x187),
+        "animation-name:left-door-9;"
+      ),
+      rightDoor[_0x316f19(0x19b)]("style", _0x316f19(0x189)),
+      contactPage[_0x316f19(0x19b)](_0x316f19(0x187), _0x316f19(0x1d7)),
+      setTimeout(function () {
+        switchToContact();
+      }, delayInMilliseconds);
+  },
+  sethire1 = () => {
+    const _0x2d084d = _0x1fd3f6;
+    (document[_0x2d084d(0x19e)](".contact>h2")[_0x2d084d(0x194)] =
+      _0x2d084d(0x19a)),
+      contactBtnNav[_0x2d084d(0x19b)](_0x2d084d(0x1a5), _0x2d084d(0x181)),
+      servicesPage[_0x2d084d(0x19b)](_0x2d084d(0x187), _0x2d084d(0x1ae)),
+      skillsPage[_0x2d084d(0x19b)](_0x2d084d(0x187), _0x2d084d(0x1ae)),
+      educationPage["setAttribute"](_0x2d084d(0x187), _0x2d084d(0x1ae)),
+      homePage[_0x2d084d(0x19b)](_0x2d084d(0x187), _0x2d084d(0x1ae)),
+      leftDoor[_0x2d084d(0x19b)](_0x2d084d(0x187), _0x2d084d(0x183)),
+      rightDoor[_0x2d084d(0x19b)]("style", _0x2d084d(0x189)),
+      contactPage["setAttribute"]("style", "z-index:\x201;");
+  },
+  switchToContact = () => {
+    const _0xa7650 = _0x1fd3f6;
+    document[_0xa7650(0x19e)](_0xa7650(0x1b2))[_0xa7650(0x194)] =
+      _0xa7650(0x1af);
+  };
+let rootColor = document[_0x1fd3f6(0x19e)](":root");
+const mainColorChange = () => {
+  const _0x1cb925 = _0x1fd3f6,
+    _0x1e6ec5 = "#ff0015",
+    _0x9c68fd = "#FF00FE",
+    _0x4208cb = _0x1cb925(0x19c),
+    _0x4ef177 = "#00ffff";
+  let _0x4cf7bd = Math[_0x1cb925(0x1d2)](Math["random"]() * 0x4) + 0x1;
+  console["log"](_0x4cf7bd);
+  switch (_0x4cf7bd) {
+    case 0x1:
+      rootColor[_0x1cb925(0x187)][_0x1cb925(0x1a1)](
+        _0x1cb925(0x1b6),
+        _0x1cb925(0x18f)
+      );
+      break;
+    case 0x2:
+      rootColor[_0x1cb925(0x187)][_0x1cb925(0x1a1)](
+        _0x1cb925(0x1b6),
+        _0x1cb925(0x18d)
+      );
+      break;
+    case 0x3:
+      rootColor[_0x1cb925(0x187)][_0x1cb925(0x1a1)](
+        _0x1cb925(0x1b6),
+        _0x1cb925(0x19c)
+      );
+      break;
+    case 0x4:
+      rootColor[_0x1cb925(0x187)]["setProperty"](
+        "--main-color",
+        _0x1cb925(0x1a9)
+      );
+      break;
+  }
+};
+let menuIcon = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1a4)),
+  navMenu = document["querySelector"](".nav-list"),
+  menuIconI = document[_0x1fd3f6(0x19e)](_0x1fd3f6(0x1cf));
+const openMenuBar1 = () => {
+    const _0x14dbbb = _0x1fd3f6;
+    navMenu[_0x14dbbb(0x19b)](
+      _0x14dbbb(0x187),
+      "animation-name:\x20menuBabAnimation-2;"
+    ),
+      menuIcon[_0x14dbbb(0x19b)](_0x14dbbb(0x1a5), _0x14dbbb(0x1d8)),
+      menuIconI[_0x14dbbb(0x19b)]("class", _0x14dbbb(0x191));
+  },
+  openMenuBar2 = () => {
+    const _0x5cbd98 = _0x1fd3f6;
+    navMenu["setAttribute"](
+      _0x5cbd98(0x187),
+      "animation-name:\x20menuBabAnimation-1;"
+    ),
+      menuIcon[_0x5cbd98(0x19b)](_0x5cbd98(0x1a5), _0x5cbd98(0x1a7)),
+      menuIconI[_0x5cbd98(0x19b)](_0x5cbd98(0x1b8), _0x5cbd98(0x1b4));
+  };
+function _0x4f4b() {
+  const _0x134139 = [
+    "animation-name:right-door-3;",
+    ".logo",
+    ".django-persent",
+    "animation-name:nodejs-pro-bar-var-2;",
+    "progressbar-animation-1",
+    ".menu-icon>i",
+    "2342364mIQlOe",
+    "animation-name:css-pro-bar-var-1;",
+    "floor",
+    ".c-persent",
+    ".nodejs-bar",
+    ".education-btn",
+    "animation-name:html-pro-bar-var-2;",
+    "z-index:\x201;",
+    "openMenuBar2()",
+    "setSkill2()",
+    ".html-persent>p",
+    "animation-name:left-door-1;",
+    ".services",
+    "animation-name:python-pro-bar-var-2;",
+    "animation-name:git-pro-bar-var-1;",
+    "animation-name:react-pro-bar-var-2;",
+    "animation-name:django-pro-bar-var-1;",
+    "animation-name:js-pro-bar-var-2;",
+    "animation-name:left-door-10;",
+    ".c-bar",
+    "animation-name:right-door-6;",
+    ".education",
+    "animation-name:left-door-5;",
+    "2531259XxYXrX",
+    "progressbar-animation-2",
+    "setContact2()",
+    ".html-bar",
+    "animation-name:left-door-9;",
+    "animation-name:right-door-2;",
+    "animation-name:left-door-6;",
+    "8xbTUYZ",
+    "style",
+    ".services-btn",
+    "animation-name:right-door-9;",
+    "animation-name:left-door-02;",
+    "classList",
+    ".nodejs-persent",
+    "#FF00FE",
+    "animation-name:left-door-7;",
+    "#ff0015",
+    "animation-name:right-door-5;",
+    "ri-menu-unfold-2-line\x20menu-i",
+    "animation-name:git-pro-bar-var-2;",
+    ".left-door",
+    "innerHTML",
+    "remove",
+    "animation-name:right-door-7;",
+    "animation-name:right-door-10;",
+    "11036640TsOzMV",
+    "setHome1()",
+    "Mail&nbsp;<span>Me</span>",
+    "setAttribute",
+    "#00ff48",
+    ".skills-btn",
+    "querySelector",
+    ".django-bar",
+    ".react-persent",
+    "setProperty",
+    "animation-name:left-door-01;",
+    ".contact",
+    ".menu-icon",
+    "onclick",
+    ".js-persent",
+    "openMenuBar1()",
+    "animation-name:right-door-8;",
+    "#00ffff",
+    "animation-name:react-pro-bar-var-1;",
+    ".css-bar",
+    "15DTpLTV",
+    "animation-name:html-pro-bar-var-1;",
+    "z-index:\x20-1;",
+    "Contact&nbsp;<span>Me</span>",
+    ".contact-btn-nav",
+    "117542tYEWbQ",
+    ".contact>h2",
+    ".hero-btn",
+    "ri-menu-fold-2-line\x20menu-i",
+    ".right-door",
+    "--main-color",
+    "setHome01()",
+    "class",
+    "animation-name:left-door-2;",
+    "6095537DMiTDo",
+    "setHome02()",
+    "3301869rNSDxm",
+    "add",
+    "18953352bQiVvN",
+    ".progressbar",
+    "setHome2()",
+    "4ohvmML",
+    "animation-name:right-door-02;",
+    "animation-name:left-door-3;",
+    ".git-bar",
+    ".python-bar",
+    "animation-name:c-pro-bar-var-2;",
+    "17dujJPl",
+    ".git-persent",
+    "animation-name:js-pro-bar-var-1;",
+  ];
+  _0x4f4b = function () {
+    return _0x134139;
+  };
+  return _0x4f4b();
+}
